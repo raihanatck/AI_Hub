04da9ec4ededb79daffa877f3f952df7bef40971
Raihan Shaikh <123348525+mraihanshaikh@users.noreply.github.com>
2024-12-06 10:55:03 +0530

    resolve delete model api issue

--- routes/modelRouter.js
+++ routes/modelRouter.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const { createModel, editModel, GetModel, GetSingleModel } = require('../controllers/modelsController');
-const { deleteModel } = require('mongoose');
+const { createModel, editModel, GetModel, GetSingleModel, DeleteModel } = require('../controllers/modelsController');
 const { upload } = require('../middlewares/uploadimage');
 const auth = require('../middlewares/auth');
 const modelRouter = express.Router();
 
 modelRouter.post('/',auth,upload.single('image'),createModel);
 modelRouter.put('/:modelid',auth,upload.single('image'),editModel);
-modelRouter.delete('/:modelid',auth,deleteModel);
+modelRouter.delete('/:modelid',auth,DeleteModel);
 modelRouter.get('/',GetModel);
 modelRouter.get('/:modelid',GetSingleModel);
 
